1299205194e48a331a2373c8af5385d428e95505
JulyKikuAkita <b92701105@gmail.com>
2021-12-24 19:28:43 -0800

    Day 68: write data to documents directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+/**
+ object-c native encoding: UTF-16
+ swift native encoding: UTF-8
+ */
 struct User: Identifiable, Comparable {
     // equal condition use Equatbale protocol
     static func < (lhs: User, rhs: User) -> Bool { // operator overloading
@@ -37,6 +41,26 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+
+                do {//atomically alwasy set to true for for thread safety (write and read the same file at the same time)
+                    try str.write(to: url, atomically: true, encoding: .utf8) //TODO: add extention to FileManager
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
 }
 

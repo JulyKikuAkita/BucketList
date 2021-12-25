f79cf8f6dc1a3cfd344fb35271ea3303d716115c
JulyKikuAkita <b92701105@gmail.com>
2021-12-24 19:05:14 -0800

    Day 68: Comparable protocol

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,7 +7,33 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    // equal condition use Equatbale protocol
+    static func < (lhs: User, rhs: User) -> Bool { // operator overloading
+        lhs.lastName < rhs.lastName
+    }
+
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
+    let values = [1, 5, 7, 0, 2].sorted()
+    let users_not_confront_Comparable_protocol = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Listner")
+    ].sorted {
+        $0.lastName < $1.lastName
+    }
+
+    let users_confront_Comparable_protocol = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Listner")
+    ].sorted()
+
     var body: some View {
         Text("Hello, world!")
             .padding()

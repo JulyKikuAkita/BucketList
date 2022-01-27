21d066828bc309f35cfa14d90dfbbbc65c093a6d
JulyKikuAkita <b92701105@gmail.com>
2022-01-26 20:59:44 -0800

    Day 69: LocalAuthentication with FaceID/TouchID

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,13 +4,44 @@
 //
 //  Created by Ifang Lee on 12/24/21.
 //
-
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+
+    //Note: always provide a fallback method, like passcode, if biometrics fail.
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data." // for touch ID
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 

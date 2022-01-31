1c76eeb29446aec00de9cad1c7d4541e85b46dab
JulyKikuAkita <i-fang.lee@grindr.com>
2022-01-30 23:39:01 -0800

    Day 71: sort with Comparable protocol

--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -15,8 +15,18 @@ struct Query: Codable {
     let pages: [Int: Page]
 }
 
-struct Page: Codable {
+struct Page: Codable, Comparable {
     let pageid: Int
     let title: String
     let terms: [String: [String]]?
+    
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+    }
+    
+    // we can call sorted() directly instead of
+    // pages = items.query.pages.values.sorted{ $0.title < $1.title }
+    static func < (lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
 }

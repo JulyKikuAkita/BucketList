0262ad991e3b72ef7dd7bf626834f6c6559d1445
JulyKikuAkita <i-fang.lee@grindr.com>
2022-01-30 23:32:17 -0800

    Day 71: Downloading data from Wikipedia

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -20,6 +20,13 @@ struct EditView: View {
     @State private var description: String
 
     var onSave: (Location) -> Void
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
 
     var body: some View {
         NavigationView {
@@ -27,6 +34,22 @@ struct EditView: View {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                Section("Nearby…") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading")
+                    case .loaded:
+                        ForEach(pages, id:\.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -40,6 +63,8 @@ struct EditView: View {
                     onSave(newLocation)
                     dismiss()
                 }
+            }.task {
+                await fetchNearbyPlaces()
             }
         }
     }
@@ -57,6 +82,57 @@ struct EditView: View {
 
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    
+    // fetch places from Wikipidia
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        // guard let url = URL(string: urlString) else {
+        guard let url = getGPSQueryUrl() else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success – convert the array values to our pages array
+            pages = items.query.pages.values.sorted{ $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
+        }
+    }
+    
+    /**
+     Generate "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+     
+     */
+    func getGPSQueryUrl() -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "en.wikipedia.org"
+        components.path = "/w/api.php"
+        components.queryItems = [
+            URLQueryItem(name: "ggscoord", value: "\(location.latitude)|\(location.longitude)"),
+            URLQueryItem(name: "action", value: "query"),
+            URLQueryItem(name: "prop", value: "coordinates|pageimages|pageterms"),
+            URLQueryItem(name: "colimit", value: "50"),
+            URLQueryItem(name: "piprop", value: "thumbnail"),
+            URLQueryItem(name: "pithumbsize", value: "500"),
+            URLQueryItem(name: "pilimit", value: "50"),
+            URLQueryItem(name: "wbptterms", value: "description"),
+            URLQueryItem(name: "generator", value: "geosearch"),
+            URLQueryItem(name: "ggsradius", value: "10000"),
+            URLQueryItem(name: "ggslimit", value: "50"),
+            URLQueryItem(name: "format", value: "json")
+        ]
+        return components.url
     }
 }
 

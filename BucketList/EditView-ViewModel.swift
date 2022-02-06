532057adb5119e5b4fd100a671a19349521291c8
JulyKikuAkita <b92701105@gmail.com>
2022-02-06 12:56:53 -0800

    add Ediview-VM

---
+++ BucketList/EditView-ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EditView-ViewModel.swift
+//  BucketList
+//
+//  Created by Ifang Lee on 2/6/22.
+//
+
+import Foundation
+import MapKit
+
+extension EditView {
+    @MainActor class EditViewModel: ObservableObject {
+    }
+}
